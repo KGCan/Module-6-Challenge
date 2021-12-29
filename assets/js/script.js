39adf3efb988d07b23ecefc5f9c88ba841e8d765
PacDraft <Kirsten@PacificDraftWorks.com>
2021-12-28 20:06:41 -0800

    added styling for current forecast, added classes for some divs, started variables for js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,24 @@
-//REVERTING TO BEGINNING TO RESUBMIT
-
 //API Key from Openweathermap (had a new one generated as I am starting fresh)
 var apiKey = "e64f1bbcc51498c175315a53f4e40804";
+
+//Variable for list of previously searched cities
+var searchList = $(".searchList");
+
+//Variable for search button
+var searchButton = $(".searchButton");
+
+//Variable for search field input
+var fieldInput = $(".fieldInput");
+
+//Variables for current city data
+var currentCity = $(".currentCity");
+var currentDate = $(".currentDate");
+var weatherIcon = $(".weatherIcon");
+var temperature = $(".temperature");
+var humidity = $(".humidity");
+var wind = $(".wind");
+var uv = $(".uv");
+
+//Variable for extended forecast
+var extendedForecast = $("extendedForecast");
 
