e78e0aa3bdd42d7d7b83ad0e5024dd74c73396e2
PacDraft <Kirsten@PacificDraftWorks.com>
2021-12-28 20:21:08 -0800

    corrected variables to pull dom references in js, fixed issue with spacing in css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,30 @@
+//API variable
+var weatherApiLink = 'https://api.openweathermap.org';
 //API Key from Openweathermap (had a new one generated as I am starting fresh)
 var apiKey = "e64f1bbcc51498c175315a53f4e40804";
 
 //Variable for list of previously searched cities
-var searchList = $(".searchList");
+var searchList = document.querySelector(".searchList");
 
 //Variable for search button
-var searchButton = $(".searchButton");
+var searchButton = document.querySelector(".searchButton");
 
 //Variable for search field input
-var fieldInput = $(".fieldInput");
+var fieldInput = document.querySelector(".fieldInput");
 
 //Variables for current city data
-var currentCity = $(".currentCity");
-var currentDate = $(".currentDate");
-var weatherIcon = $(".weatherIcon");
-var temperature = $(".temperature");
-var humidity = $(".humidity");
-var wind = $(".wind");
-var uv = $(".uv");
+var currentCity = document.querySelector(".currentCity");
+var currentDate = document.querySelector(".currentDate");
+var weatherIcon = document.querySelector(".weatherIcon");
+var temperature = document.querySelector(".temperature");
+var humidity = document.querySelector(".humidity");
+var wind = document.querySelector(".wind");
+var uv = document.querySelector(".uv");
 
 //Variable for extended forecast
 var extendedForecast = $("extendedForecast");
 
+var cityDate = new Date ();
+let day = String(cityDate.getDate()).padStart(2, '0');
+let month = String(cityDate.getMonth() + 1).padStart(2, '0');
+let year = cityDate.getFullYear();
